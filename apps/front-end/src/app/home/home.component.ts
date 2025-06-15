94df7b097899f6122ec58f54167f4c4e8ede6f65
Avshalom-Ts <avshalomz.ts@gmail.com>
2025-06-15 15:42:29 +0000

    feat: Enhance HomeComponent with store creation and fetching functionality

--- apps/front-end/src/app/home/home.component.ts
+++ apps/front-end/src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { environment } from '../../environments/environments.prod';
 import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 
 
 export interface WelcomeData {
@@ -11,9 +12,10 @@ export interface WelcomeData {
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
+  standalone: true,
 })
 export class HomeComponent implements OnInit {
   API_BASE_URL = environment.PRODUCTION
@@ -22,19 +24,59 @@ export class HomeComponent implements OnInit {
 
   apiConnection = signal<WelcomeData | null>(null)
 
-  constructor(private http: HttpClient) { }
+  storeForm!: FormGroup;
+
+  allStores = signal<WelcomeData[]>([]);
+
+  constructor(
+    private http: HttpClient,
+    private fb: FormBuilder
+  ) {
+  }
 
 
   ngOnInit() {
     console.log("🚀 ~ NxWelcomeComponent ~ environment.production:", environment.PRODUCTION)
     console.log("🚀 ~ NxWelcomeComponent ~ API_BASE_URL:", this.API_BASE_URL)
     this.getData();
+
+    this.storeForm = this.fb.group({
+      name: new FormControl(''),
+      description: new FormControl(''),
+    });
+    this.getAllStores();
   }
 
   getData() {
     this.http.get<WelcomeData>(`${this.API_BASE_URL}`).subscribe((response) => {
       console.log("🚀 ~ HomeComponent ~ this.http.get<WelcomeData> ~ response:", response)
       this.apiConnection.set(response);
+    });
+  }
+
+  onSubmit() {
+    console.log("🚀 ~ HomeComponent ~ onSubmit ~ this.storeForm.value:", this.storeForm.value);
+    this.http.post<WelcomeData>(`${this.API_BASE_URL}/store-name`, this.storeForm.value).subscribe({
+      next: (response) => {
+        console.log("🚀 ~ HomeComponent ~ onSubmit ~ response:", response);
+        this.storeForm.reset();
+        this.allStores.update((stores) => [...stores, response]);
+      },
+      error: (error) => {
+        console.error("🚀 ~ HomeComponent ~ onSubmit ~ error:", error);
+      }
+    });
+  }
+
+  getAllStores() {
+    this.http.get<WelcomeData[]>(`${this.API_BASE_URL}/store-name`).subscribe({
+      next: (response) => {
+        console.log("🚀 ~ HomeComponent ~ getAllStores ~ response:", response);
+        this.allStores.set(response);
+      },
+      error: (error) => {
+        console.error("🚀 ~ HomeComponent ~ getAllStores ~ error:", error);
+      }
     });
   }
 }
